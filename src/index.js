64bca9a725022695135c6e30b0b90cf82ae4bfee
sergelen <sergelen020@gmail.com>
2021-05-18 12:05:20 +0800

    h

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Profile from './profile'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Profile />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/profile.js
+++ src/profile.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import './profile.scss'
 
 
 function Profile() {
     return (
-        <div>
+        <div className="cont">
+            <div className="top flex center">
+                <div className="pro-cont flex perfect center">
+                    <i className="large material-icons">account_circle</i>
+                </div>
+            </div>
+            <ul class="collection flex column">
+                <li class="collection-item flex perfect"><i className="material-icons">person</i><input type="" placeholder="Username" /></li>
+                <li class="collection-item flex perfect"><i className="material-icons">date_range</i><input type="" placeholder="Birthday" /></li>
+                <li class="collection-item flex perfect"><i className="material-icons">phone</i><input type="" placeholder="Phone number" /></li>
+                <li class="collection-item flex perfect"><i className="material-icons">mail</i><input type="" placeholder="Instagram account" /></li>
+                <li class="collection-item flex perfect"><i className="material-icons">mail</i><input type="" placeholder="Email" /></li>
+                <li class="collection-item flex perfect"><i className="material-icons">visibility</i><input type="" placeholder="Password" /></li>
+            </ul>
+            <div className="flex center">
+                <a class="waves-effect btn">Save profile</a>
+            </div>
             
         </div>
     )
